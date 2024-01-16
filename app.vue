dc9dd650aacb72425916d61e548cc4dff0d8cdd8
Virenbar <Virenbar@users.noreply.github.com>
2024-01-16 20:54:03 +0500

    chore: add error page

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@ const logo = "https://e621-top.github.io/img/logo-top.png";
 useHead({
   titleTemplate: (title) => (title ? `${title} - ` : "") + "e621-top",
   meta: [
-    { name: "description", content: "Automatic e621 tag top" }
+    { name: "description", content: "Automatically updated artist and character tag tops of e621.net" }
   ],
   htmlAttrs: { lang: "en", "data-bs-theme": "dark" }
 });
@@ -13,11 +13,11 @@ useSeoMeta({
   ogTitle: "621-top",
   ogSiteName: "621-top",
   ogUrl: "https://e621-top.github.io/",
-  ogDescription: "Automatic e621 tag top",
+  ogDescription: "Automatically updated artist and character tag tops of e621.net",
   ogImage: logo,
   ogImageSecureUrl: logo,
   twitterTitle: "e621-top",
-  twitterDescription: "Automatic e621 tag top",
+  twitterDescription: "Automatically updated artist and character tag tops of e621.net",
   twitterImage: logo,
   twitterCard: "summary"
 });
