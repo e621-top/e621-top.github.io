9caa60d1e180ba6dbbf136ab6af29c168edd7103
Virenbar <Virenbar@users.noreply.github.com>
2024-06-17 19:35:39 +0500

    chore: bump dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,24 @@
+import { execSync } from "child_process";
+
+const exec = (command: string) => execSync(command).toString().trim();
+const branch = exec("git branch --show-current");
+const hash = exec("git rev-parse HEAD");
+const date = new Date().toISOString();
+
 export default defineNuxtConfig({
   css: [
     "@/assets/css/styles.scss"
   ],
   modules: [
+    "@nuxt/eslint",
     ["nuxt-gtag", { id: "G-2RY5LX8LQK" }]
   ],
   runtimeConfig: {
     public: {
       repository: "https://github.com/e621-top/e621-top.github.io",
-      branch: process.env.HEAD || "master",
-      hash: process.env.COMMIT_REF || "unknown",
-      date: new Date().toISOString()
+      branch,
+      hash,
+      date
     }
   }
 });
