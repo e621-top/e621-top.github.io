e97f7fed560cd0783975ec5a203a7125c864696a
Virenbar <Virenbar@users.noreply.github.com>
2025-07-24 23:24:50 +0500

    chore: comments

--- composables/useAbsoluteURL.ts
+++ composables/useAbsoluteURL.ts
@@ -1,5 +1,4 @@
 export default function (path?: string) {
-  // if (!import.meta.server || !import.meta.client) return;
   if (!path) {
     path = useRoute().path;
   }
@@ -7,13 +6,4 @@ export default function (path?: string) {
     path = path?.startsWith('/') ? `${useRuntimeConfig().app.baseURL}${path.slice(1)}` : path;
   }
   return new URL(path, useAppConfig().url).toString();
-  /*
-  if (import.meta.client) {
-    const t = useAppConfig().url;
-    const route = useRoute();
-    return new URL(route.path, t).toString();
-  } */
-
-  // path = path?.startsWith('/') ? `${useRuntimeConfig().app.baseURL}${path.slice(1)}` : path;
-  // return url.protocol + '//' + url.host + path;
 };
